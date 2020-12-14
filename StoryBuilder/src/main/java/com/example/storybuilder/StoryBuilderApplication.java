99673f0cc44fc7fc80f11c02b2437a74ea6e30f2
Craig Pounds <nweaver@windstream.net>
2020-12-14 11:19:29 -0600

    Initial commit

---
+++ StoryBuilder/src/main/java/com/example/storybuilder/StoryBuilderApplication.java
@@ -0,0 +1,34 @@
+package com.example.storybuilder;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class StoryBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryBuilderApplication.class, args);
+	}
+	@Bean
+	public WebSecurityConfigurerAdapter webSecurityConfig(DataSource dataSource) {
+		return new WebSecurityConfigurerAdapter() {
+			@Override
+			protected void configure(HttpSecurity http) throws Exception {
+				http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().formLogin();
+			}
+
+			@Override
+			protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+				builder.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder()).dataSource(dataSource);
+			}
+			
+		};
+	}
+}
---
+++ StoryBuilder/src/test/java/com/example/storybuilder/StoryBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storybuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
